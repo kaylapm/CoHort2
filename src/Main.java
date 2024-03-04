2a5c4d786c44860146b4cdbec45880d0ec9b5979
Kayla <kaylaputrimaharani@gmail.com>
2024-03-04 14:53:16 +0700

    hallo

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main{
     public static void main(String[] args){
         System.out.println ("Hello World!, Hi Kayla Maharani, Programming is so much fun!");
-<<<<<<< HEAD
-=======
         System.out.println ("Assalamualaikum");
         System.out.println (23*5);
->>>>>>> f8af19e (DasProg1)
+        System.out.println (23*6);
     }
 }
